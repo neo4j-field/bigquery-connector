9c9b96edc37d3902a494b524d3e995be0f5db701
Dave Voutila <dave.voutila@neotechnology.com>
2023-03-02 17:21:56 -0500

    latest proc example

--- proc.sql
+++ proc.sql
@@ -1,43 +1,35 @@
 CREATE OR REPLACE PROCEDURE
-  `neo4j-se-team-201905.bqr_neo4j_demo.neo4j_gds_graph_project`(
-	    graph_name STRING,
-	    graph_json STRING,
-	    neo4j_db_name STRING,
-	    neo4j_user STRING,
-	    neo4j_password STRING,
-	    neo4j_host STRING,
-	    bq_project STRING,
-	    bq_dataset STRING,
-	    node_tables STRING,
-	    edge_tables STRING
-	  )
-	WITH CONNECTION `neo4j-se-team-201905.eu.voutila-bq-spark-eu`
-	OPTIONS (
-		    description="Project a graph from BigQuery into Neo4j AuraDS or GDS.",
-		    engine="SPARK",
-		    runtime_version="2.0",
-		    container_image="eu.gcr.io/neo4j-se-team-201905/neo4j-bigquery-connector:0.2.0"
-	)
+  `neo4j-se-team-201905.bqr_neo4j_demo.neo4j_gds_graph_project`(graph_name STRING,
+    graph_uri STRING,
+    neo4j_secret STRING,
+    bq_project STRING,
+    bq_dataset STRING,
+    node_tables ARRAY<STRING>,
+    edge_tables ARRAY<STRING>)
+WITH CONNECTION `neo4j-se-team-201905.eu.voutila-bq-spark-eu` OPTIONS (engine='SPARK',
+    runtime_version='2.0',
+    container_image='eu.gcr.io/neo4j-se-team-201905/neo4j-bigquery-connector:0.3.0',
+    properties=[],
+    description="Project a graph from BigQuery into Neo4j AuraDS or GDS.")
+  LANGUAGE python AS R"""
+# Need to back out this Spark BigQuery Support crap.
+import sys
+print(f"original path: {sys.path}")
+newpath = [p for p in sys.path if not "spark-bigquery-support" in p]
+sys.path = newpath
+print(f"new path: {sys.path}")
 
-	LANGUAGE PYTHON AS r"""
-	# Need to back out this Spark BigQuery Support crap.
-	import sys
-	print(f"original path: {sys.path}")
-	newpath = [p for p in sys.path if not "spark-bigquery-support" in p]
-	sys.path = newpath
-	print(f"new path: {sys.path}")
+from pyspark.sql import SparkSession
+from templates import BigQueryToNeo4jGDSTemplate
 
-	from pyspark.sql import SparkSession
-	from templates import BigQueryToNeo4jGDSTemplate
+spark = (
+      SparkSession
+      .builder
+      .appName("Neo4j BigQuery Connector")
+      .getOrCreate()
+)
 
-	spark = (
-		    SparkSession
-		    .builder
-		    .appName("Neo4j BigQuery Connector")
-		    .getOrCreate()
-	)
-
-	template = BigQueryToNeo4jGDSTemplate()
-	args = template.parse_args()
-	template.run(spark, args)
-	""";
+template = BigQueryToNeo4jGDSTemplate()
+args = template.parse_args()
+template.run(spark, args)
+""";
